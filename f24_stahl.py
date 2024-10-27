0bf73bffc7cfc90248d938a7a9f6fe6e09ad5b47
Jude Stahl <JUD2183062@maricopa.edu>
2024-10-27 09:49:37 -0700

    Added f24_stahl homework files

---
+++ f24_stahl.py
@@ -0,0 +1,5 @@
+print("The reason of the unreason with which my reason is\n\
+afflicted so weakens my reason that with reason\n\
+I murmur at your beauty;")
+
+print("\nDon Quixote by Miguel de Cervantes")
