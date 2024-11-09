78e29070064ffe8f211025a17bbbdf56cea0904c
Alex Johnson <ale2156957@maricopa.edu>
2024-11-09 16:26:13 -0700

    Module 8 Project Commit 01

---
+++ F24_Johnson.md
@@ -0,0 +1,2 @@
+"One day I feel I’m on top of the world, and the next it’s falling in on me. I can get back on. I can get back on."
+(Rush - Far Cry (2007))
---
+++ F24_Johnson.py
@@ -0,0 +1,4 @@
+print("One day I feel I’m on top of the world")
+print("And the next it’s falling in on me")
+print("I can get back on")
+print("I can get back on")
