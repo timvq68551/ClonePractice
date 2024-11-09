ad51b967b85baf8e37d10ac77d1e8ce4550ae415
ERN2145842 <ern2145842@maricopa.edu>
2024-11-09 16:40:44 -0700

    first commit

---
+++ f24_La_Mertha.py
@@ -0,0 +1,8 @@
+def quote():
+    return('''"There's no such thing as a pinnacle. 
+When you reach the top, there's still more way above. 
+When you say you see a pinnacle yourself...
+That's when you stopped walking."
+(The Eminence in Shadow, Daisuke Aizawa)''')
+    
+print(quote())
