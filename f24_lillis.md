d04f839f733ec3051029068f5d85e88706a72e2c
Michael Lillis <mickf82161@maricopa.edu>
2024-11-09 21:17:22 -0700

    Readme and Python Script added

---
+++ f24_lillis.md
@@ -0,0 +1,6 @@
+# Movie Quote
+
+> You either die a hero, or you live long enough to see yourself become the villain.
+
+\- Harvey Dent
+(The Dark Knight, 2008)
---
+++ f24_lillis.py
@@ -0,0 +1,11 @@
+"""
+Michael Lillis
+CIS256
+Module 08 Programming Project
+Movie Quote
+"""
+
+print("Movie Quote\n")
+print('"You either die a hero, or you live long enough to see yourself become the villain."\n')
+print("- Harvey Dent")
+print("(The Dark Knight, 2008)")
